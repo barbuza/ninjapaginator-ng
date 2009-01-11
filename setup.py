61be6d0fda0dcb48f05cf1b4e7da7ec34bc40f99
Anderson <self.anderson@gmail.com>
2009-01-11 17:47:05 +0200

    easy_install installation added

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+setup(
+    name = "django-ninjapaginator",
+    version = "0.1",
+    packages = find_packages(),
+    author = "Anderson",
+    author_email = "self.anderson@gmail.com",
+    description = "Django application with multiple type of pagination integrated",
+    license = "BSD",
+    keywords = "django",
+    url = "http://www.assembla.com/wiki/show/ninja-paginator",
+    install_requires = ["django-annoying"],
+    include_package_data = True,
+    zip_safe = False
+)
+
