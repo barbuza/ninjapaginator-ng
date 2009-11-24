7a8e5b0749d9582e32a4655245a698794f108cb0
Victor Kotseruba <barbuzaster@gmail.com>
2009-11-24 18:08:29 +0300

    templates splited, digg style should works

---
+++ ninjapaginator/templatetags/paginator.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from django.template import Library
+
+register = Library()
+
+
+@register.simple_tag
+def page_link(num_page, params):
+    link = '?page=%s' % num_page
+    if params:
+        return '%s&amp;%s' (link, params.replace('&', '&amp;'))
+    return link
+    
--- ninjapaginator/util.py
+++ ninjapaginator/util.py
@@ -26,15 +26,17 @@ class NinjaPaginator(object):
     http://bitbucket.org/offline/django-annoying/wiki/Home
     """
 
-    def __init__(self, object_list='object_list', style=None, per_page=10, frame_size=8):
+    def __init__(self, object_list='object_list', style=None, per_page=10, frame_size=8,
+                 allow_user_per_page=False):
         """
         receive decorator parameters 
         """
         self.object_list = object_list
-        self.style = style or getattr(settings, "PAGINATION_STYLE", "digg")
+        style = style or getattr(settings, "PAGINATION_STYLE", "digg")
+        self.style = getattr(self, '%s_style' % style)
         self.per_page = per_page
-        self.per_page_backup = per_page
         self.frame_size = frame_size
+        self.allow_user_per_page = allow_user_per_page
         
     def __call__(self, function):
         """
@@ -47,80 +49,92 @@ class NinjaPaginator(object):
         """
         style pagination according to 'style' parameter
         """
-        self.output = self.function(request, *args, **kwargs)
-        if not isinstance(self.output, dict):
-            return self.output
+        
+        output = self.function(request, *args, **kwargs)
+        
+        if not isinstance(output, dict):
+            return output
         
         params = request.GET.copy()
         
+        page_num = 1
         try:
-            self.page_num = int(params.pop('page')[0])
+            page_num = int(params.pop('page')[0])
         except (ValueError, KeyError):
-            self.page_num = 1
-
-        if "per_page" in params:
-            self.per_page = int(params['per_page'])
-        else:
-            # reset per_page variable to default
-            self.per_page = self.per_page_backup
-            params['per_page'] = self.per_page
-            
-        extra_params = unicode_urlencode(params)
-
-        self.paginate_qs = self.output.pop(self.object_list)
-        self.paginator = Paginator(self.paginate_qs, self.per_page)
+            pass
+        
+        per_page = self.per_page
+        if self.allow_user_per_page and 'per_page' in params:
+            try:
+                per_page = int(params['per_page'])
+            except (ValueError, KeyError):
+                params['per_page'] = self.per_page
+        elif 'per_page' in params:
+            params.pop('per_page')
+        
+        paginate_qs = output.pop(self.object_list)
+        paginator = Paginator(paginate_qs, per_page)
+        
         try:
-            self.page = self.paginator.page(self.page_num)
+            page = paginator.page(page_num)
         except EmptyPage:
             raise  Http404()
-        self.output['page_num'] = self.page_num
-        self.output['per_page'] = self.per_page
-        self.output['extra_params'] = extra_params
-        self.pages = self.paginator.num_pages
-        self.output[self.object_list] = self.page.object_list
-        self.output[self.style] = True
-        run_style = getattr(self, "%s_style" % self.style)
-        return run_style()
-    
-    def digg_style(self):
-        if self.page_num > 1:
-            self.output['PREVIOUS'] = self.page_num -1
-        if self.page_num < self.paginator.num_pages:
-            self.output['NEXT'] = self.page_num + 1
-        if self.pages > self.frame_size and self.pages <= self.frame_size +2:
-            self.output['left_page_numbers'] = range(1, self.pages + 1)
-        elif self.pages < self.frame_size:
-            self.output['left_page_numbers'] = range(1, self.pages + 1)
-        elif self.pages > self.frame_size and self.page_num < self.frame_size - 1:
-            self.output['left_page_numbers'] = range(1, self.frame_size + 1)
-            self.output['right_page_numbers'] = range(self.pages -1, self.pages +1)
-        elif self.pages > self.frame_size and self.page_num > self.frame_size - 2 and self.pages - (self.frame_size / 2) <= self.page_num + 1:
-            self.output['left_page_numbers'] = range(1, 3)
-            self.output['middle_page_numbers'] = range(self.pages - self.frame_size + 1, self.pages +1)
-        elif self.pages > self.frame_size and self.page_num > self.frame_size - 2:
-            self.output['left_page_numbers'] = range(1, 3)
-            self.output['middle_page_numbers'] = range(self.page_num - (self.frame_size / 2) +1, self.page_num + (self.frame_size / 2))
-            self.output['right_page_numbers'] = range(self.pages -1, self.pages +1)
-        return self.output
+        
+        output['page_num'] = page_num
+        output['per_page'] = per_page
+        output['params'] = unicode_urlencode(params)
+        pages = paginator.num_pages
+        output[self.object_list] = page.object_list
+        output['paginator_%s' % self.style] = True
+        
+        output.update(self.style(pages, page_num))
+        return output
 
-    def filmfeed_style(self):
-        if self.pages < self.frame_size:
-            self.output['page_numbers'] = range(1, self.pages + 1)
-        elif self.page_num < (self.frame_size / 2) + 1:
-            self.output['page_numbers'] = range(1, self.frame_size + 1)
-        elif self.page_num >= (self.frame_size / 2) + 1 and self.pages - (self.frame_size / 2) <= self.page_num:
-            self.output['page_numbers'] = range(self.pages - self.frame_size + 1, self.pages + 1)
-        elif self.page_num >= (self.frame_size / 2) + 1:
-            start = self.page_num - (self.frame_size / 2)
-            end = self.page_num + (self.frame_size / 2)
-            self.output['page_numbers'] = range(start, end + 1)
-        return self.output
     
-    def muzx_style(self):
+    def digg_style(self, pages, page_num):
+        output = {}
+        if page_num > 1:
+            output['PREVIOUS'] = page_num -1
+        if page_num < pages:
+            output['NEXT'] = page_num + 1
+        if pages > self.frame_size and pages <= self.frame_size +2:
+            output['left_page_numbers'] = range(1, pages + 1)
+        elif pages <= self.frame_size:
+            output['left_page_numbers'] = range(1, pages + 1)
+        elif pages > self.frame_size and page_num < self.frame_size - 1:
+            output['left_page_numbers'] = range(1, self.frame_size + 1)
+            output['right_page_numbers'] = range(pages -1, pages +1)
+        elif pages > self.frame_size and page_num > self.frame_size - 2 and pages - (self.frame_size / 2) <= page_num + 1:
+            output['left_page_numbers'] = range(1, 3)
+            output['middle_page_numbers'] = range(pages - self.frame_size + 1, pages +1)
+        elif pages > self.frame_size and page_num > self.frame_size - 2:
+            output['left_page_numbers'] = range(1, 3)
+            output['middle_page_numbers'] = range(page_num - (self.frame_size / 2) +1, page_num + (self.frame_size / 2))
+            output['right_page_numbers'] = range(pages -1, pages +1)
+        return output
+    
+    
+    def filmfeed_style(self, pages, page_num):
+        output = {}
+        if pages < self.frame_size:
+            output['page_numbers'] = range(1, pages + 1)
+        elif page_num < (self.frame_size / 2) + 1:
+            output['page_numbers'] = range(1, self.frame_size + 1)
+        elif page_num >= (self.frame_size / 2) + 1 and pages - (self.frame_size / 2) <= page_num:
+            output['page_numbers'] = range(pages - self.frame_size + 1, pages + 1)
+        elif page_num >= (self.frame_size / 2) + 1:
+            start = page_num - (self.frame_size / 2)
+            end = page_num + (self.frame_size / 2)
+            output['page_numbers'] = range(start, end + 1)
+        return output
+    
+    
+    def muzx_style(self, pages, page_num):
+        output = {}
         side_size = int(self.frame_size / 2.0)
-        left_plus = max(0, side_size - (self.pages - self.page_num))
-        right_plus = max(0, side_size - (self.page_num - 1))
-        prev_pages = range(1, self.page_num)[-1 * (side_size + left_plus):]
-        next_pages = range(self.page_num + 1, self.pages + 1)[:side_size + right_plus]
-        self.output['page_numbers'] = prev_pages + [self.page_num] + next_pages
-        return self.output
+        left_plus = max(0, side_size - (pages - page_num))
+        right_plus = max(0, side_size - (page_num - 1))
+        prev_pages = range(1, page_num)[-1 * (side_size + left_plus):]
+        next_pages = range(page_num + 1, pages + 1)[:side_size + right_plus]
+        output['page_numbers'] = prev_pages + [page_num] + next_pages
+        return output
